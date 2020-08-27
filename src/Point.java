9bebf53091b8f1ad3608daf566e6877427c19454
alex94chen <alex94chen@gmail.com>
2020-08-27 14:54:53 +0300

    added function add

--- src/Point.java
+++ src/Point.java
@@ -5,7 +5,15 @@ public class Point
 			//david: add default constructor and p. constructor
 		
 			//alex: add function: public int add(PointOther);
-			
+	public Point add(Point Other) {
+	
+		Point newPoint = new Point();
+		
+		newPoint.x=this.x+Other.x;
+		newPoint.y=this.y+Other.y;
+				
+		return newPoint;
+	}
 			//or: add function: public int multPoints(Point other);
 		
 			//guy:add function: public String toString();
