600158fb1498e18696df76cbb462856a916ca411
David <win96x10@gmail.com>
2020-08-27 14:55:02 +0300

    def.vri & d.cons & p.cons

--- src/Point.java
+++ src/Point.java
@@ -1,9 +1,21 @@
 
 public class Point
 	{
-			//david: add fields: int x, int y
-			//david: add default constructor and p. constructor
-		
+	
+	//david: add fields: int x, int y
+	private int x, y;
+
+	//david: add default constructor and p. constructor
+	public Point() {
+		x = 0;
+		y = 0;
+	}
+	public Point(int _x, int _y) {
+		x = _x;
+		y = _y;
+	}
+	
+	
 			//alex: add function: public int add(PointOther);
 			
 			//or: add function: public int multPoints(Point other);
