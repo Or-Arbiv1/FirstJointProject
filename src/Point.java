c33572ce8e94f7043cf1d0356fb51bce756d4c55
Guy rahamim <guy.danino5@gmail.com>
2020-08-27 14:11:02 +0300

    initial Commit

---
+++ src/Point.java
@@ -0,0 +1,12 @@
+
+public class Point
+	{
+			//david: add fields: int x, int y
+			//david: add default constructor and p. constructor
+		
+			//alex: add function: public int add(PointOther);
+			
+			//or: add function: public int multPoints(Point other);
+		
+			//guy:add function: public String toString();
+	}
