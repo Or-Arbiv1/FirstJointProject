4fd1e9835a95ef81751fd78e3926a6664ac57216
Guy rahamim <guy.danino5@gmail.com>
2020-08-27 14:50:09 +0300

    Added toString function.

--- src/Point.java
+++ src/Point.java
@@ -4,9 +4,13 @@ public class Point
 			//david: add fields: int x, int y
 			//david: add default constructor and p. constructor
 		
-			//alex: add function: public int add(PointOther);
+			//alex: add function: public Point add(PointOther);
 			
-			//or: add function: public int multPoints(Point other);
+			//or: add function: public Point multPoints(Point other);
 		
 			//guy:add function: public String toString();
+			public String toString()
+			{
+				return "x: " +x + "\t y: " +y;
+			}
 	}
